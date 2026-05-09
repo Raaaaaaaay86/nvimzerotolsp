4a48f89e40b53a483206658d6cbb7e593b1578a9
RayLin <>
2026-05-09 10:38:09 +0800

    feat(ufo): update nvim/ufo setting

--- lua/plugins/utils.lua
+++ lua/plugins/utils.lua
@@ -34,8 +34,7 @@ return {
 			vim.o.foldlevel = 99
 			vim.o.foldlevelstart = 99
 			vim.o.foldenable = true
-			vim.keymap.set("n", "zR", require("ufo").openAllFolds)
-			vim.keymap.set("n", "zM", require("ufo").closeAllFolds)
+			vim.o.fillchars = 'eob: ,fold: ,foldopen:,foldsep: ,foldinner: ,foldclose:'
 			require("ufo").setup()
 		end,
 	},
